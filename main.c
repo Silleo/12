ba420c71a3e4e5833b6c6262c5383d6b63c37619
Горбенко Семен Юрьевич <ip913s06@awp07.csc.local>
2020-02-13 15:02:38 +0700

    Create empty main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
-
+    printf("Hello World");
+    system("pause");
     return 0;
 }
 
